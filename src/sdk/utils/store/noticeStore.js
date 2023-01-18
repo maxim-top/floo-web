c4c32ae424c9d6e7f71587a41a22daeeed04812e
ZouJinHai <jinhai@bmxlabs.com>
2023-01-18 16:30:07 +0800

    Remove noticeStore.saveNotice implementation

--- src/sdk/utils/store/noticeStore.js
+++ src/sdk/utils/store/noticeStore.js
@@ -1,14 +1,14 @@
-import { getItem, saveItem } from './storeBase';
+import { getItem } from './storeBase';
 
 const noticeStore = {
-  saveNotice: (meta) => {
-    const items = getItem('key_notice_store') || [];
+  saveNotice: (/*meta*/) => {
+    /*const items = getItem('key_notice_store') || [];
     items.push(meta);
     const len = items.length;
     if (len > 50) {
       items.splice(0, len - 50);
     }
-    saveItem('key_notice_store', items);
+    saveItem('key_notice_store', items);*/
   },
   getNotice: () => getItem('key_notice_store') || [],
   removeNotice: (/*meta*/) => {
